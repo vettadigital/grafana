678364dbfcb44e456012a589e3c7dd29c22b7332
Steven Vachon <contact@svachon.com>
2020-08-25 15:47:27 -0400

    Select timezone from dashboard toolbar instead of settings

--- packages/grafana-e2e/src/flows/setDashboardTimeRange.ts
+++ packages/grafana-e2e/src/flows/setDashboardTimeRange.ts
@@ -1,18 +1,30 @@
 import { e2e } from '../index';
+import { selectOption } from './selectOption';
 
 export interface DashboardTimeRangeConfig {
   from: string;
   to: string;
+  zone?: string;
 }
 
-export const setDashboardTimeRange = ({ from, to }: DashboardTimeRangeConfig) =>
+export const setDashboardTimeRange = ({ from, to, zone }: DashboardTimeRangeConfig) =>
   e2e.pages.Dashboard.Toolbar.navBar().within(() => {
     e2e()
       .get('[aria-label="TimePicker Open Button"]')
       .click();
+
+    if (zone) {
+      e2e()
+        .contains('button', 'Change time zone')
+        .click();
+      selectOption(e2e.pages.Dashboard.Settings.General.timezone(), zone, false);
+    }
+
+    // For smaller screens
     e2e()
       .get('[aria-label="TimePicker absolute time range"]')
       .click();
+
     e2e()
       .get('[aria-label="TimePicker from field"]')
       .clear()
